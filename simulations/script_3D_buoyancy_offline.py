cf656ff2e61f2ae67bf07bcdb55b54d10b4a04d9
Alexander Kier Christensen <akc17@ic.ac.uk>
2019-03-12 15:42:18 +0000

    Added 'blob' initial particle distribution to script_3D_buoyancy_offline.py

--- simulations/script_3D_buoyancy_offline.py
+++ simulations/script_3D_buoyancy_offline.py
@@ -4,16 +4,17 @@ import os
 import numpy as np
 from datetime import timedelta
 
+np.random.seed(1234)
 
 # Set simulation parameters
 os.chdir("/media/alexander/AKC Passport 2TB/Maarten/sim022/")#("/media/alexander/DATA/Ubuntu/Maarten/sim022")
 filenames = "F*n.nc.022"
-savepath = "/home/alexander/Desktop/temp_maarten/hpc_test"#os.path.join(os.getcwd(), "/data/trajectories")
+savepath = "/home/alexander/Desktop/temp_maarten/0.001x100"#os.path.join(os.getcwd(), "/data/trajectories")
 scale_fact = 1200 #5120./3
-num_particles = 1000
-runtime = timedelta(seconds=1)
-dt = timedelta(seconds=0.05)
-outputdt = timedelta(seconds=0.1)
+num_particles = 10000
+runtime = timedelta(seconds=0.1)
+dt = timedelta(seconds=0.001)
+outputdt = timedelta(seconds=0.001)
 
 # Set up parcels objects.
 timestamps = extract_timestamps(filenames)
@@ -37,15 +38,20 @@ fieldset.add_periodic_halo(zonal=True, meridional=True, halosize=10)
 
 # Generate initial particle density field
 pfield_grid = RectilinearZGrid(lon=np.arange(0, 720), lat=np.arange(0, 720), depth=np.arange(0, 360), time=np.zeros(1), mesh='flat')
-pfield_data = np.ones((pfield_grid.ydim, pfield_grid.xdim))
-pfield = Field(name='uniform_initial_dist', data=pfield_data, grid=pfield_grid)
+uniform_data = np.ones((pfield_grid.ydim, pfield_grid.xdim))
+conc_data = np.zeros((pfield_grid.ydim, pfield_grid.xdim))
+conc_data[150:210,150:210] = np.ones((60, 60))
+conc_data[330:390,330:390] = np.ones((60, 60))
+conc_data[510:570,510:570] = np.ones((60, 60))
+pfield_uniform = Field(name='uniform_initial_dist', data=uniform_data, grid=pfield_grid)
+pfield_conc = Field(name='concentrated_initial_dist', data=conc_data, grid=pfield_grid)
 
 # Initiate particleset & kernels
 pclass = Generic3D
 pset = ParticleSet.from_field(fieldset=fieldset,
                               pclass=pclass,
-                              start_field=pfield,
-                              depth=np.random.rand(num_particles) * 360,
+                              start_field=pfield_uniform,
+                              depth=np.random.rand(num_particles) * 180,
                               size=num_particles)
 for particle in pset:
     particle.diameter = np.random.uniform(0.018, 0.032)
@@ -62,8 +68,8 @@ pset.execute(kernels,
 
 print("\nFinished with %d particles left at endtime." % pset.size)
 
-print("\nReformatting output for animations.")
-reformat_for_animate(savepath)
+# print("\nReformatting output for animations.")
+# reformat_for_animate(savepath)
 
 print("\nDone.\n")
 

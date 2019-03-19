34055da3d9a23026f3d65b8160ccbc37ff7c568c
Alexander Kier Christensen <akc17@ic.ac.uk>
2019-03-19 17:37:50 +0000

    1) Added extract_vorticities() method to add vorticities to netCDF files.

--- simulations/util/util.py
+++ simulations/util/util.py
@@ -3,9 +3,11 @@ import netCDF4
 from glob import glob
 from tqdm import tqdm
 import os
+import time
+from parcels import Field
 
 
-__all__ = ['rand_unit_vect_3D', 'extract_timestamps', 'find_max_velocities']
+__all__ = ['rand_unit_vect_3D', 'extract_timestamps', 'find_max_velocities', 'extract_vorticities', 'uniform_init_field', 'conc_init_field']
 
 
 def rand_unit_vect_3D():
@@ -79,3 +81,95 @@ def find_max_velocities(filepaths):
         nc.close()
 
     return max_velocities
+
+
+def extract_vorticities(filepaths):
+    """
+    A method to open netCDF files containing velocity data, calculate the vorticities in the
+    x, y and z direction, and save the result as a new netCDF file.
+    :param filepaths: String or list of strings representing the path(s) to the file(s).
+    """
+    paths = sorted(glob(str(filepaths))) if not isinstance(filepaths, list) else filepaths
+
+    if len(paths) == 0:
+        notfound_paths = filepaths
+        raise IOError("FieldSet files not found: %s" % str(notfound_paths))
+
+    for fp in tqdm(paths):
+        if not os.path.exists(fp):
+            raise IOError("FieldSet file not found: %s" % str(fp))
+
+        nc = netCDF4.Dataset(fp)
+
+        nc_new = netCDF4.Dataset(fp[:-4] + "_vort.022", "w", format="NETCDF4")
+        nc_new.createDimension('Nz', nc.dimensions['Nz'].size)
+        nc_new.createDimension('Ny', nc.dimensions['Ny'].size)
+        nc_new.createDimension('Nx', nc.dimensions['Nx'].size)
+        nc_new.createVariable('u', nc.variables["u"].dtype, ('Nz', 'Ny', 'Nx'))
+        nc_new.createVariable('v', nc.variables["v"].dtype, ('Nz', 'Ny', 'Nx'))
+        nc_new.createVariable('w', nc.variables["w"].dtype, ('Nz', 'Ny', 'Nx'))
+        nc_new.createVariable('t01', nc.variables["t01"].dtype, ('Nz', 'Ny', 'Nx'))
+        nc_new.createVariable('time', nc.variables["time"].dtype, ())
+        nc_new.createVariable('vort_u', np.float32, ('Nz', 'Ny', 'Nx'))
+        nc_new.createVariable('vort_v', np.float32, ('Nz', 'Ny', 'Nx'))
+        nc_new.createVariable('vort_w', np.float32, ('Nz', 'Ny', 'Nx'))
+
+        nc_new.set_auto_mask(False)
+
+        u = nc.variables["u"][:]
+        v = nc.variables["v"][:]
+        w = nc.variables["w"][:]
+        time = nc.variables["time"][:]
+        t01 = nc.variables["t01"][:]
+
+        du_dz, du_dy, du_dx = np.gradient(u)
+        dv_dz, dv_dy, dv_dx = np.gradient(v)
+        dw_dz, dw_dy, dw_dx = np.gradient(w)
+
+        vort_u = dw_dy - dv_dz
+        vort_v = du_dz - dw_dx
+        vort_w = dv_dx - du_dy
+
+        nc_new.variables['u'][:] = u
+        nc_new.variables['v'][:] = v
+        nc_new.variables['w'][:] = w
+        nc_new.variables['time'][:] = time
+        nc_new.variables['t01'][:] = t01,
+        nc_new.variables['vort_u'][:] = vort_u
+        nc_new.variables['vort_v'][:] = vort_v
+        nc_new.variables['vort_w'][:] = vort_w
+
+        nc_new.sync()
+
+        nc.close()
+        nc_new.close()
+
+
+def uniform_init_field(grid):
+    """Produce a uniform distribution over the whole domain.
+
+    :param grid: Grid on which to define distribution.
+
+    :returns pfield: OceanParcels Field object containing the distribution.
+    """
+    uniform_data = np.ones((grid.ydim, grid.xdim))
+    pfield = Field(name='uniform_initial_dist', data=uniform_data, grid=grid)
+
+    return pfield
+
+
+def conc_init_field(grid):
+    """Produce a uniform distribution over the whole domain.
+
+    :param grid: Grid on which to define distribution.
+
+    :returns pfield: OceanParcels Field object containing the distribution.
+    """
+    conc_data = np.zeros((grid.ydim, grid.xdim))
+    conc_data[150:210, 150:210] = np.ones((60, 60))
+    conc_data[330:390, 330:390] = np.ones((60, 60))
+    conc_data[510:570, 510:570] = np.ones((60, 60))
+    pfield = Field(name='concentrated_initial_dist', data=conc_data, grid=grid)
+
+
+    return pfield

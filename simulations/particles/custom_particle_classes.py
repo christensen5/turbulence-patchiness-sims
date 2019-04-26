b9c16c50505b9f277d4647e749778b726124167d
Alexander Kier Christensen <akc17@ic.ac.uk>
2019-04-26 10:25:05 +0100

    Constant custom variables now written only once rather than every timestep.
    Custom variable 'temp' currently not written at all, since temp-dependence not yet implemented.

--- simulations/particles/custom_particle_classes.py
+++ simulations/particles/custom_particle_classes.py
@@ -6,15 +6,15 @@ __all__ = ['Generic3D', 'Akashiwo3D']
 
 
 class Generic3D(JITParticle):
-    temp = Variable('temp', dtype=np.float32, initial=0.)
-    diameter = Variable('diameter', dtype=np.float32, initial=None)
+    temp = Variable('temp', dtype=np.float32, initial=0., to_write=False)
+    diameter = Variable('diameter', dtype=np.float32, initial=None, to_write='once')
 
 
 class Akashiwo3D(JITParticle):
-    temp = Variable('temp', dtype=np.float32, initial=0.)
-    diameter = Variable('diameter', dtype=np.float32, initial=None)
-    v_swim = Variable('v_swim', dtype=np.float32, initial=0.)
+    temp = Variable('temp', dtype=np.float32, initial=0., to_write=False)
+    diameter = Variable('diameter', dtype=np.float32, initial=None, to_write='once')
+    v_swim = Variable('v_swim', dtype=np.float32, initial=0., to_write='once')
     dir_x = Variable('dir_x', dtype=np.float32, initial=0.)
     dir_y = Variable('dir_y', dtype=np.float32, initial=0.)
     dir_z = Variable('dir_z', dtype=np.float32, initial=0.)
-    B = Variable('B', dtype=np.float32, initial=2.0)
+    B = Variable('B', dtype=np.float32, initial=2.0, to_write='once')

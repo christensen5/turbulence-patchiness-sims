119dd09c76065c127bca7ae6cf0edbbb5fa2337b
Alexander Kier Christensen <akc17@ic.ac.uk>
2019-09-11 15:30:30 +0100

    Added MPI profiling script for CX1.

--- simulations/analysis/analysis_tools/script_density_multiprocessing.py
+++ simulations/analysis/analysis_tools/script_density_multiprocessing.py
@@ -9,7 +9,7 @@ def calc_kde(data):
     return kde(data.T)
 
 timestamps = np.linspace(0, 300, 31, dtype=int).tolist()
-filepath = "/media/alexander/DATA/Ubuntu/Maarten/outputs/sim022/initunif/dead/trajectories_10000p_30s_0.01dt_0.1sdt_initunif_dead"
+filepath = "/media/alexander/DATA/Ubuntu/Maarten/outputs/sim022/initunif/mot/100000p_30s_0.01dt_0.1sdt_2.0B_initunif_mot_1.0vswim/trajectories_100000p_30s_0.01dt_0.1sdt_2.0B_initunif_mot_1.0vswim"
 
 # verify timestamp format
 if isinstance(timestamps, int) or isinstance(timestamps, float):
@@ -20,27 +20,35 @@ lons = np.load(str(filepath + "_lons.npy"))
 lats = np.load(str(filepath + "_lats.npy"))
 deps = np.load(str(filepath + "_deps.npy"))
 
+assert timestamps[-1] - (lons.shape[0]-1) < 1e-4  # rough check that timestamps span entire sim data
+
 if timestamps == "firstlast":
     timestamps = [0, lons.shape[0]-1]
 
 # compute and plot densities
 xmin, ymin, zmin = 0, 0, 0
 xmax, ymax, zmax = 720, 720, 360
-density = np.zeros((100, 100, 50, len(timestamps)))
-for t in tqdm(range(len(timestamps))):
-    x = lons[timestamps[t], ~np.isnan(lons[timestamps[t], :])]
-    y = lats[timestamps[t], ~np.isnan(lats[timestamps[t], :])]
-    z = deps[timestamps[t], ~np.isnan(deps[timestamps[t], :])]
-    xyz = np.vstack([x, y, z])
-    kde = stats.gaussian_kde(xyz)
+nx, ny, nz = 72, 72, 36
 
-    xi, yi, zi = np.mgrid[xmin:xmax:100j, ymin:ymax:100j, zmin:zmax:50j]
-    coords = np.vstack([item.ravel() for item in [xi, yi, zi]])
+density = np.zeros((nx, ny, nz, len(timestamps)))
 
-    # Multiprocessing
-    cores = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=cores)
-    results = pool.map(calc_kde, np.array_split(coords.T, 2))
-    density[:, :, :, t] = np.concatenate(results).reshape(xi.shape)
+N = len(timestamps)
+with tqdm(total=N) as pbar:
+    for t in range(N):
+        x = lons[timestamps[t], ~np.isnan(lons[timestamps[t], :])]
+        y = lats[timestamps[t], ~np.isnan(lats[timestamps[t], :])]
+        z = deps[timestamps[t], ~np.isnan(deps[timestamps[t], :])]
+        xyz = np.vstack([x, y, z])
+        kde = stats.gaussian_kde(xyz)
+        xi, yi, zi = np.mgrid[xmin:xmax:nx*1j, ymin:ymax:ny*1j, zmin:zmax:nz*1j]
+        coords = np.vstack([item.ravel() for item in [xi, yi, zi]])
 
-np.save("/media/alexander/DATA/Ubuntu/Maarten/outputs/sim022/initunif/dead/density_10000p_30s_0.01dt_0.1sdt_initunif_dead/density.npy", density)
+        # Multiprocessing
+        cores = 4
+        pool = multiprocessing.Pool(processes=cores)
+        results = pool.map(calc_kde, np.array_split(coords.T, 2))
+        density[:, :, :, t] = np.concatenate(results).reshape(xi.shape)
+
+        pbar.update(1)
+
+np.save(filepath + '_density.npy', density)

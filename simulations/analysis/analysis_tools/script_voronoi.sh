eca187d36e8f545eecfc642ebe9eddbb2c36ffae
Alexander Kier Christensen <akc17@ic.ac.uk>
2020-04-09 13:24:24 +0100

    Fixes to voronoi analysis scripts, which will no longer chop off the bottom of the sim, and which should work with both "increasing-depth" and "decreasing-depth" z-directions.

--- simulations/analysis/analysis_tools/script_voronoi.sh
+++ simulations/analysis/analysis_tools/script_voronoi.sh
@@ -3,7 +3,7 @@ cd $1
 
 find . -regextype "egrep" \
        -iregex './vor/in/vor_mm_[0-9][0-9][0-9].txt' \
-       -exec voro++ -o -px -py -c "%v" 0 600 0 600 -1 $2 {} \;
+       -exec voro++ -o -px -py -c "%v" 0 600 0 600 0 300 {} \;
        
 cd ./vor/in
 
